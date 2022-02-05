88fd0f4d94d66f9619afb7c4fa93c8c051f12f86
Enrique Folte <hhfolte@gmail.com>
2022-02-05 16:26:04 -0500

    add get move method and position identifier methods

--- board.rb
+++ board.rb
@@ -1,4 +1,19 @@
 class Board
     def initialize
         @grid = Array.new(3){Array.new(3,"O")}
+    end
+
+    def get_move
+        puts "Please choose a square to reveal, eg '1,2'"
+        response = gets.chomp.to_i
+    end
+
+    def [](pos)
+        row, col = pos
+        @grid[row][col]
+    end
+
+    def []=(pos, val)
+        row, col = pos
+        @grid[row][col] = val
 end
